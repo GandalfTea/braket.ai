fcfb5a46a564b386b544d0c6f9e5a4b794ead1c7
gandalftea <octavian.concept@gmail.com>
2021-12-15 15:58:46 +0000

    init files for python API, no work done

---
+++ api/braket.c
@@ -0,0 +1,14 @@
+
+
+
+// Python library used to interact with .dataset files or braket.ai webservers.
+// It should allow the user to interact with datasets, request them and feed them into the training loop
+// tutorial : https://docs.python.org/3/extending/extending.html
+
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+static PyObject* request_dataset(PyObject *self, PyObject *args) {
+    const char* command;
+}
+
